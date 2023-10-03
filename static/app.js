c893fc2c778902cd8df97861bb7f858c1469e215
Daniel Rhodes <danielrhodes47@gmail.com>
2023-10-03 08:33:36 -0500

    tested api

---
+++ static/app.js
@@ -0,0 +1,9 @@
+// add infinite scroll
+//  or add pagination
+// offset
+// integer
+// The index of the first result to return. Use with limit to get the next page of search results.
+
+// Example value: 5
+// Default value: 0
+// Range: 0 - 1000
--- test_api.py
+++ test_api.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from models import db, User
 from config.test_config import *
 from app import app, CURR_USER_KEY
-from api import get_token  # Import the get_token function
+from api import get_token, get_genres, get_songs_by_genre, is_valid_spotify_id
 
 os.environ["FLASK_ENV"] = "testing"
 
@@ -24,11 +24,27 @@ class TestSpotifyApi(TestCase):
         with app.app_context():
             db.drop_all()
 
-    # Mock the get_token function
-    def test_get_token(self, mock_get_token):
-        # Call the function under test
+    def test_get_token(self):
+        """Test get_token function."""
         token = get_token()
+        self.assertIsInstance(token, str)
 
-        # Assertions
-        self.assertIsNotNone(token)
-        self.assertEqual(token, 'mocked_token')
+    def test_get_genres(self):
+        """Test get_genres function."""
+        token = get_token()
+        genres = get_genres(token)
+        self.assertIsInstance(genres, list)
+
+    def test_get_songs_by_genre(self):
+        """Test get_songs_by_genre function."""
+        token = get_token()
+        tracks = get_songs_by_genre(token, "country")
+        self.assertIsInstance(tracks, list)
+
+    def test_is_valid_spotify_id(self):
+        """Test is_valid_spotify_id function."""
+        token = get_token()
+        valid_id = "6rqhFgbbKwnb9MLmUQDhG6"
+        invalid_id = "invalid_id"
+        self.assertTrue(is_valid_spotify_id(token, valid_id))
+        self.assertFalse(is_valid_spotify_id(token, invalid_id))
